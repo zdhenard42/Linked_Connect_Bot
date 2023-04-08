a571d6877ae2564ef63008faebf6e62b7401f2c5
Zachary Henard <zdhenard42@tntech.edu>
2023-04-07 23:27:58 -0500

    Fixed minor bugs

--- MyConnectmenu.py
+++ MyConnectmenu.py
@@ -16,7 +16,7 @@ def main():
 
     # Feel free to comment out the three lines below and uncomment the fourth one if you would like to watch the process!
     myoptions = ChromeOptions()
-    myoptions.add_argument("--headless")
+    #myoptions.add_argument("--headless")
     driver = uc.Chrome(options=myoptions)
     # driver = uc.Chrome
     
@@ -61,37 +61,50 @@ def chose_Connect(driver):
     k = 10
     print("\nBeginning connection request process...\nThere is a delay between requests intentionally to bypass bot detections")
     while i < maxConnect:
-        urllink = "https://www.linkedin.com/search/results/people/?&keywords="+Keywords+"&network=%5B\"S\"%2C\"O\"%5D&origin=SWITCH_SEARCH_VERTICAL&page="+str(k)+"&sid=aiC&spellCorrectionEnabled=true"
+        try:
+            # Construct the URL for the search results page
+            urllink = f"https://www.linkedin.com/search/results/people/?&keywords={Keywords}&network=%5B%22S%22%2C%22O%22%5D&origin=SWITCH_SEARCH_VERTICAL&page={k}&sid=aiC&spellCorrectionEnabled=true"
             
-        driver.get(urllink)
-        time.sleep(2)
-
-        all_buttons = driver.find_elements(By.TAG_NAME,"button")
-        connect_buttons = [btn for btn in all_buttons if btn.text == "Connect"]
-
-        ## Iterating over all the buttons. 
-
-        for btn in connect_buttons:
-            driver.execute_script("arguments[0].click();", btn)
-            name = driver.find_element(By.XPATH,"/html/body/div[3]/div/div/div[2]/p/span/strong").text
-            print("Sending connection request to ",name)
-            # This waits between 4 and 15 seconds per connection request
-            time.sleep(randint(4,15))
-            send = driver.find_element(By.XPATH,"//button[@aria-label='Send now']")
-            driver.execute_script("arguments[0].click();", send)
-            # Added Cause some people only prefer connection if you have their official email.
-            close = driver.find_element(By.XPATH, "//button[@aria-label='Dismiss']")
-            driver.execute_script("arguments[0].click();", close)
-            time.sleep(randint(4,15))
+            # Load the search results page and wait for it to load
+            driver.get(urllink)
             time.sleep(2)
+
+            # Find all the Connect buttons on the page
+            all_buttons = driver.find_elements(By.TAG_NAME, "button")
+            connect_buttons = [btn for btn in all_buttons if btn.text == "Connect"]
+
+            # Loop through all the Connect buttons and send connection requests
+            for btn in connect_buttons:
+                driver.execute_script("arguments[0].click();", btn)
+                name = driver.find_element(By.XPATH, "/html/body/div[3]/div/div/div[2]/p/span/strong").text
+                print(f"Sending connection request to {name}")
+                time.sleep(randint(4, 15))
+                send = driver.find_element(By.XPATH, "//button[@aria-label='Send now']")
+                driver.execute_script("arguments[0].click();", send)
+                close = driver.find_element(By.XPATH, "//button[@aria-label='Dismiss']")
+                driver.execute_script("arguments[0].click();", close)
+                time.sleep(randint(4, 15))
+                time.sleep(2)
+
+            # If there are no more Connect buttons on the page, go to the next page
             if len(connect_buttons) == 0:
-                k+=1
+                k += 1
             else:
-                i+=len(connect_buttons)
-                k+=1
-        print("Connection Invitations sent = ", i)
-    driver.quit()
-    exit(0)
+                # Update the counter for the number of connections made and go to the next page
+                i += len(connect_buttons)
+                k += 1
+
+            # Print the total number of connection requests sent so far
+            print(f"Connection Invitations sent = {i}")
+
+            # Exit the loop if the maximum number of connections has been reached
+            if i >= maxConnect:
+                break
+
+    # Handle any exceptions that may arise during the process
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+            break
 
 
 def chose_withdraw(driver):
@@ -120,4 +133,5 @@ def chose_withdraw(driver):
     driver.quit()
     exit(0)
 
-main()
+if __name__ == "__main__":
+    main()
